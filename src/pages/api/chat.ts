a4c06a473afc35bc9d55e172b1848c6f64315a17
Nico Weber <100025848@mvla.net>
2023-04-08 15:45:43 -0700

    test

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,32 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   if (req.method === 'POST') {
     // Get the request data as JSON
     const data = req.body;
-
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: data}],
+    });
+    console.log(completion.data.choices[0].message);
     // Do something with the data
     const response_data = {
       message: 'Received POST request with data:',
-      data: data
+      data: completion.data.choices[0].message,
+      name:'r'
     }
-
+    console.log(req.body)
     // Return a JSON response
     res.status(200).json(response_data);
   } else {
-    res.status(405).send('Method Not Allowed');
+    res.status(405).send({name:"Illegal"});
   }
-
-  console.log(req.query)
-
-  res.status(200).json(req.body)
   
 }
--- src/pages/simple-chat.tsx
+++ src/pages/simple-chat.tsx
@@ -3,8 +3,22 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
 
+  async function handleButtonClick() {
+    const url = '/api/chat';
+    const data = 'hola quiero cafe'
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+    const responseData = await response.json();
+    console.log(responseData)
+  }
+
+  handleButtonClick()
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
