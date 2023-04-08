323cf35ebb9448606194e253970b2c5beb7e02d5
Jason Wu <jasonwu700@gmail.com>
2023-04-08 14:24:45 -0700

    front-end setup

---
+++ src/pages/_app.jsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import Home from './index.jsx'
+
+export default function Index() {
+    return (
+        <Home/>
+      )
+}
+  
---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar.jsx'
+
+export default function Home() {
+    return (
+        <>
+            <header>
+
+            </header>
+            <main>
+                <div>
+
+                </div>
+                
+            </main>
+        </>
+    )
+}
