263a214ac665712012f7bf4ee87254e4561e2d4b
Da_Dog <jiawenli233@gmail.com>
2023-04-08 16:37:04 -0700

    feat: speech to text and text to speech (stt.ts/tts.ts)

---
+++ src/pages/api/stt.ts
@@ -0,0 +1,31 @@
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+
+async function speechToText(language) {
+    const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SPEECH_REGION);
+    speechConfig.speechRecognitionLanguage = language;
+
+    let audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
+    let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+
+    speechRecognizer.recognizeOnceAsync(result => {
+        switch (result.reason) {
+            case sdk.ResultReason.RecognizedSpeech:
+                break;
+            case sdk.ResultReason.NoMatch:
+                console.log("NOMATCH: Speech could not be recognized.");
+                break;
+            case sdk.ResultReason.Canceled:
+                const cancellation = sdk.CancellationDetails.fromResult(result);
+                console.log(`CANCELED: Reason=${cancellation.reason}`);
+
+                if (cancellation.reason == sdk.CancellationReason.Error) {
+                    console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+                    console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+                    console.log("CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+        speechRecognizer.close();
+        return result.text;
+    });
+}
