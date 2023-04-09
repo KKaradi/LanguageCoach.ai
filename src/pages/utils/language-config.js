c7fe347dacc44464e5b5d87aa97d9e5ce1d3b143
Kai Karadi <kaikaradi23@gmail.com>
2023-04-08 20:36:24 -0700

    feature: multi lang support

--- src/pages/utils/language-config.js
+++ src/pages/utils/language-config.js
@@ -20,11 +20,35 @@ export const languageConfig = {
             },
           ]
     },
-    // "Chinese":{
-    //     "code":
-    // },
-    // "French":{
-    //     "code":
-    // },
+    "Chinese":{
+        "code":"zh-CN",
+        "seed":[
+          {
+            role: "system",
+            content:
+              "You are a Mandarin-Chinese chat bot roleplaying as a Mandarin-Chinese Barista. The user will submit messages in Mandarin-Chinese and as an assistant your job is to respond.",
+          },
+        ]
+    },
+    "French":{
+        "code":"fr-FR",
+        "seed":[
+          {
+            role: "system",
+            content:
+              "You are a French chat bot roleplaying as a French Barista. The user will submit messages in French and as an assistant your job is to respond.",
+          },
+        ]
+    },
+    "Japanese":{
+        "code":"ja-JP",
+        "seed":[
+          {
+            role: "system",
+            content:
+              "You are a Japanese chat bot roleplaying as a Japanese Barista. The user will submit messages in Japanese and as an assistant your job is to respond.",
+          },
+        ]
+    }
 
 }
