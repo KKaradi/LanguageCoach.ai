06eaec94ef570ee5ff7f399129386d85c07858eb
Kai Karadi <kaikaradi23@gmail.com>
2023-04-09 00:32:20 -0700

    changes

--- src/pages/components/InputField.jsx
+++ src/pages/components/InputField.jsx
@@ -19,7 +19,7 @@ export default function InputField({submitHandler, resetHandler, languageCode}) 
 
     return ( 
         <form onSubmit={onSubmitHandler} onReset={onResetHandler} action="">
-            <textarea ref={textbox} name="" id="" cols="30" rows="10" />
+            <textarea ref={textbox} name="" id="" cols="30" rows="10" placeholder='Type a message here or record one below.'/>
             <OptionBar submitHandler={submitHandler} languageCode={languageCode}/>
         </form>
     )
