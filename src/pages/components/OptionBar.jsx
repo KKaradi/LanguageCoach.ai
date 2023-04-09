de27fa8d97aa0edfcea379e8ac363d3a0f54dff6
Da_Dog <jiawenli233@gmail.com>
2023-04-08 17:25:51 -0700

    feat: speech to text

--- src/pages/components/OptionBar.jsx
+++ src/pages/components/OptionBar.jsx
@@ -1,8 +1,16 @@
+import speechToText from "@/pages/api/stt";
+import {useState} from "react";
 
 export default function OptionBar() {
+    const [recording, setRecording] = useState(false);
+
     return (
         <div className="optionBar">
-            <button>Record</button>
+            <button onClick={(e) => {
+                e.preventDefault();
+                speechToText("en-us", setRecording);
+                setTimeout(() => {setRecording(true)}, 800);
+            }}>{recording ? "Recording..." : "Record"}</button>
             <input type="submit" value="Send" />
         </div>
     )
