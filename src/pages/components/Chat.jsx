b3d289a320bcbd783b08cca0f6fe63caa58c64e8
Kai Karadi <kaikaradi23@gmail.com>
2023-04-08 17:24:03 -0700

    chat

--- src/pages/components/Chat.jsx
+++ src/pages/components/Chat.jsx
@@ -1,34 +1,62 @@
-import Message from './Message.jsx'
-import UserInputField from './InputField.jsx'
-import Dropdown from './Dropdown.jsx'
-import { fetchMessages } from '../utils/FetchDataUtil.js'
-import InputField from './InputField.jsx';
-import { useState, useEffect} from 'react'
+import Message from "./Message.jsx";
+import UserInputField from "./InputField.jsx";
+import Dropdown from "./Dropdown.jsx";
+import InputField from "./InputField.jsx";
+import { useState, useEffect } from "react";
+
+async function createCompletion(conversation, setConversation) {
+  console.log("creating completion");
+  try {
+    console.log('messages before',conversation)
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ conversation }),
+    });
+
+    const json = await response.json();
+    const responseConversation = json.conversation;
+    console.log('response conver',responseConversation)
+    if (responseConversation !== undefined) {
+      setConversation(responseConversation);
+    }
+    // console.log('g',response,messages)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const ORDER_A_DRINK_CONVERSATION_SEED = [
+  {
+    role: "system",
+    content:
+      "You are a Spanish chat bot roleplaying as a Spanish Barista. The user will submit messages in Spanish and as an assistant your job is to respond.",
+  },
+];
 
 //[{role:"user","system","assistant", content:"string"}]
 export default function Chat() {
-    const [messages, setMessages] = useState([]);
-    // fetchMessages().then(r => setMessages(r));
-    
-    useEffect(() =>{
-        fetchMessages().then(r => setMessages(r));
-        console.log(messages);
-    }, [])
+  const [conversation, setConversation] = useState(ORDER_A_DRINK_CONVERSATION_SEED);
 
+  useEffect(() => {
+    createCompletion(conversation, setConversation);
+  }, []);
 
-    return (
-        <>
-            <Dropdown />
-            <div className='chat'>
-                <div className='chatDisplay'>
-                    {messages.map( msg => (
-                        <Message body={msg} />
-                    ))}
-                </div>
-                <div className="userInputField">
-                    <InputField />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Dropdown />
+      <div className="chat">
+        <div className="chatDisplay">
+          {conversation.map((msg,indx) => (
+            <Message key={indx} body={msg} />
+          ))}
+        </div>
+        <div className="userInputField">
+          <InputField />
+        </div>
+      </div>
+    </>
+  );
 }
